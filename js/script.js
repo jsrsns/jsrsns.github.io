b02f815bf436cc0c9f52bdd2d8db898bcd04eac2
Joes Roosens <joesroosens@gmail.com>
2016-08-03 14:56:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 /* Random background image */
 var bgImageArray = ['/images/Stairs.svg','/images/Ladder.svg','/images/Boxes.svg','/images/Bricks.svg','/images/Flag.svg','/images/Beam.svg','/images/Wall.svg','/images/Floor.svg'],
-  selectBG = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
+  selectBG1 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
+  selectBG2 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
+  selectBG3 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
+  selectBG4 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
   
 
-$('#top-left,#top-right,#bottom-left,#bottom-right').css('background-image', 'url(' + selectBG + ')')
+$('#top-left').css('background-image', 'url(' + selectBG1 + ')');
+$('#top-right').css('background-image', 'url(' + selectBG2 + ')')
+$('#bottom-left').css('background-image', 'url(' + selectBG3 + ')')
+$('#bottom-right').css('background-image', 'url(' + selectBG4 + ')')
 });
 
 /* Joes Roosens - jsrsns.be */
