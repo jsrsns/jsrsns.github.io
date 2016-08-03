9b91c4505aefe35e48ab7c144967f624ba8a3274
Joes Roosens <joesroosens@gmail.com>
2016-08-03 19:42:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   
 /* Random background image */
-var images = ['/images/Stairs.svg', '/images/Wall.svg', '/images/Floor.svg', '/images/Ladder.svg', '/images/Flag.svg', '/images/Beam.svg', '/images/Boxes.svg', '/images/Bricks.svg'];
+var images = ['/images/Beam.svg', '/images/Boxes.svg', '/images/Bricks.svg', '/images/Corner.svg', '/images/Files.svg', '/images/Flag.svg', '/images/Floor.svg', '/images/Folds.svg', '/images/Ladder.svg', '/images/Pillar.svg', '/images/Stairs.svg', '/images/Wall.svg'];
 
 var changeBG = function() {
   var bgStart = images[Math.floor(Math.random() * images.length)];
