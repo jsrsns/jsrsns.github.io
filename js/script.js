9c4d3a2c3a57f20583459bdb4b76d754e22dbaea
Joes Roosens <joesroosens@gmail.com>
2016-08-03 14:59:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 /* Random background image */
-var bgImageArray = ['/images/Stairs.svg','/images/Ladder.svg','/images/Boxes.svg','/images/Bricks.svg','/images/Flag.svg','/images/Beam.svg','/images/Wall.svg','/images/Floor.svg'],
+var bgImageArray = ['/images/Beam.svg','/images/Big Stairs.svg','/images/Boxes.svg','/images/Bricks.svg','/images/Corner.svg','/images/Files.svg','/images/Flag.svg','/images/Floor.svg','/images/Ladder.svg','/images/Pillar.svg','/images/Stairs.svg','/images/Wall.svg','/images/Wide Stairs.svg'],
   selectBG1 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
   selectBG2 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
   selectBG3 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
