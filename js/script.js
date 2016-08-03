04387a4e77716e64d57db5fe6e5846affd49b77c
Joes Roosens <joesroosens@gmail.com>
2016-08-03 16:28:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
 /* Random background image */
-var bgImageArray = ['/images/Beam.svg','/images/Big_Stairs.svg','/images/Boxes.svg','/images/Bricks.svg','/images/Corner.svg','/images/Files.svg','/images/Flag.svg','/images/Floor.svg','/images/Ladder.svg','/images/Pillar.svg','/images/Stairs.svg','/images/Wall.svg','/images/Wide_Stairs.svg'],
-  selectBG1 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
-  selectBG2 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
-  selectBG3 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
-  selectBG4 = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
-  
+var colors = ['#e6e6fa', '#f0e68c', '#6495ed', '#f8f8fa', '#0000cd', '#20b2aa', '#800000', '#ff4500'];
 
-$('#top-left').on('mouseover', function(){
-  $(this).css('background-image' , 'url(' + selectBG1 + ')')
-});
-$('#top-right').css('background-image', 'url(' + selectBG2 + ')')
-$('#bottom-left').css('background-image', 'url(' + selectBG3 + ')')
-$('#bottom-right').css('background-image', 'url(' + selectBG4 + ')')
+var changeBG = function() {
+  var bgStart = colors[Math.floor(Math.random() * colors.length)];
+  $(this).css('background-color', bgStart);
+};
+var el = $('#top-left, #top-right, #bottom-left, #bottom-right');
+$.each(el, changeBG);
+el.on('mouseenter', changeBG);
 });
 
 /* Joes Roosens - jsrsns.be */
