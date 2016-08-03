f9fe061b568ebfc042f34e2c724f3edbafc1bbeb
Joes Roosens <joesroosens@gmail.com>
2016-08-03 14:53:47 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var bgImageArray = ['/images/Stairs.svg','/images/Ladder.svg','/images/Boxes.svg
   selectBG = bgImageArray[Math.floor(Math.random() * bgImageArray.length)];
   
 
-$('#top-left','#top-right','#bottom-left','#bottom-right').css('background-image', 'url(' + selectBG + ')')
+$('#top-left,#top-right,#bottom-left,#bottom-right').css('background-image', 'url(' + selectBG + ')')
 });
 
 /* Joes Roosens - jsrsns.be */
