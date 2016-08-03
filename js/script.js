c25bdb85dcb8a1154ed5d854be31652a86c488ce
Joes Roosens <joesroosens@gmail.com>
2016-08-03 14:30:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
+var bgImageArray = ['/images/Stairs.svg','/images/Ladder.svg','/images/Boxes.svg','/images/Bricks.svg','/images/Flag.svg','/images/Beam.svg','/images/Wall.svg','/images/Floor.svg']
+
 /* Joes Roosens - jsrsns.be */
