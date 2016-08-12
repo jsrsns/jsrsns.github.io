604cea327b828e67c25b49d711428d914c5728c2
Joes Roosens <joesroosens@gmail.com>
2016-08-12 14:45:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
   
 /* Navigation */
 
-var link = $('#nav > li'),        // 0. work 1. about me 3. blog
-    pages = $('#content > div'),  // 0. work 1. about me 3. (...)
+var link = $('#nav > li'),
+    pages = $('#content > div'),
     close = $('.close');
     
 var wide = 'calc(100vh - 170px)',
     mobile = '100vh',
     width = $(window).width();
     
-console.log(width);
+console.log(width); // log width to console (for debug only)
 
 if (width <= '755') {
   var frame = mobile;
@@ -50,22 +50,22 @@ var fade = function() {
 close.on('click', fade);
   
 /* Random background image */
-var images = ['../img/Beam.svg', '../img/Boxes.svg', '../img/Corner.svg', '../img/Files.svg', '../img/Flag.svg', '../img/Floor.svg', '../img/Folds.svg', '../img/Ladder.svg', '../img/Pillar.svg', '../img/Stairs.svg', '../img/Wall.svg'];
+var images = ['../img/Beam.svg', '../img/Boxes.svg', '../img/Corner.svg', '../img/Files.svg', '../img/Flag.svg', '../img/Floor.svg', '../img/Folds.svg', '../img/Ladder.svg', '../img/Pillar.svg', '../img/Stairs.svg', '../img/Wall.svg']; // collection of images
 
 var changeBG = function() {
-  var bgStart = images[Math.floor(Math.random() * images.length)];
-  $(this).css('background-image', 'url(' + bgStart + ')');
+  var bgStart = images[Math.floor(Math.random() * images.length)]; // get a random image from collection
+  $(this).css('background-image', 'url(' + bgStart + ')'); // set the selected image as background image
 };
 var el = $('#top-left, #top-right, #bottom-left, #bottom-right');
 $.each(el, changeBG);
-el.on('mouseenter', changeBG);
+el.on('mouseenter', changeBG); // run changeBG on every mouseenter event for all divs
 
 /* Random logo on mobile */
-var images = ['../img/Beam.svg', '../img/Boxes.svg', '../img/Corner.svg', '../img/Files.svg', '../img/Flag.svg', '../img/Floor.svg', '../img/Folds.svg', '../img/Ladder.svg', '../img/Pillar.svg', '../img/Stairs.svg', '../img/Wall.svg'];
+var images = ['../img/Beam.svg', '../img/Boxes.svg', '../img/Corner.svg', '../img/Files.svg', '../img/Flag.svg', '../img/Floor.svg', '../img/Folds.svg', '../img/Ladder.svg', '../img/Pillar.svg', '../img/Stairs.svg', '../img/Wall.svg']; // collection of images
 
 var RandomLogo = function () {
-  var randOlogo = images[Math.floor(Math.random() * images.length)];
-  $('.random').attr('src',randOlogo);
+  var randOlogo = images[Math.floor(Math.random() * images.length)]; // get a random image from collection
+  $('.random').attr('src',randOlogo); // set the selected image as the logo
 }
 RandomLogo();
 });
